0638c64a859fc8be0409026faedee05d55772991
张明君 | Cindy Jun Zhang <vendingmachinesfullofsnacks@outlook.com>
2021-05-21 17:55:47 -0400

    Add files via upload

---
+++ 2021, SPRING/CS 392/PROGRAMMING ASSIGNMENTS/03/pfind/perms.h
@@ -0,0 +1,15 @@
+/*******************************************************************************
+ * Name        : pfind.h
+ * Author      : Jack Schneiderhan and Cindy Zhang
+ * Date        : March 14th 2021
+ * Description : Permission File.
+ * Pledge      : I pledge my honor that I have abided by the Stevens Honor System.
+ ******************************************************************************/
+
+#ifndef perms_h_
+#define perms_h_
+
+bool perm_dir(char* directory, char* perms);
+int counterr();
+
+#endif
