be4123539dbc3a76a59db1c4e25375bb4cf61e3e
Shindi <vendingmachinesfullofsnacks@outlook.com>
2022-05-29 13:38:35 -0400

    Update stringUtils.js

--- 2022, SPRING/CS 546/Lab 02/Revised/stringUtils.js
+++ 2022, SPRING/CS 546/Lab 02/Revised/stringUtils.js
@@ -1,121 +1,79 @@
+/*
+    Helper Function takes in an String Parameter.
+    Checks if the string is a valid string.
+    Returns a boolean.
+*/
+const validString = function validString(string) {
+    // -- Error Checking --
+    if(!string) throw ": Missing parameter 'string'.";
+    if(typeof(string) != "string") throw ": Incorrect type, expecting string.";
+    if(string.replace(/\s/g, '').length == 0) throw ": Input Invalid, string is empty.";
+
+    return true;
+}
+
+/*
+    Function camelCase takes in an String Parameter.
+    Returns a camelCase version of the string.
+*/
 const camelCase = function camelCase(string) {
-    if(string === undefined) {
-        throw new Error("Existential Error: String does not exist");
-    }   
-    else if (typeof(string) != "string") {
-        throw new Error("Type Error: Parameter not a string.");
-    }
-    else if (string.length == 0){
-        throw new Error("Length Error: String is empty.");
-    }
-    else if (string.replace(/\s/g, '') == '') {
-        throw new Error("Length Error: String is full of spaces.");
-    }
-    else {
-        const splitStrings = string.split(' ');
-        // console.log(splitStrings);
-        
-        var camelString = '';
-        for(i in splitStrings) {
-            if(i != 0) {
-                var word = splitStrings[i].charAt(0).toUpperCase() + splitStrings[i].slice(1).toLowerCase();
-            }
-            else {
-                var word = splitStrings[i].toLowerCase();
-            }
-            camelString = camelString.concat('', word);
-        }
+    // -- Error Checking --
+    if(!string) throw ": Missing parameter 'string'";
+    if(!validString(string)) throw ": Invalid String.";
+    
+    // -- Camel Casing --
+    let camelString = string.split(' ').reduce((a, b) => a + b.charAt(0).toUpperCase() + b.slice(1).toLowerCase(), '');
 
-        return camelString;
-    }
+    return camelString.charAt(0).toLowerCase() + camelString.slice(1);
 }
 
-// console.log(camelCase('my function rocks'));        // Returns: "myFunctionRocks"
-// console.log(camelCase('FOO BAR'));                  // Returns: "fooBar"
-// console.log(camelCase("How now brown cow"));        // Returns: "howNowBrownCow"
-// console.log(camelCase());                       // Throws Error
-// console.log(camelCase('         '));                     // Throws Error
-// console.log(console.log(camelCase(123)));       // Throws Error
-// console.log(camelCase(["Hello", "World"]));     // Throws Error
-
+/*
+    Function replaceChar takes in an String Parameter.
+    Replace any characters in the string that are the same as the starting character.
+    Replace with alternating '*' and '$' characters.
+    Returns a string with replacements.
+*/
 const replaceChar = function replaceChar(string) {
-    if(string === undefined) {
-        throw new Error("Existential Error: String does not exist");
-    }   
-    else if (typeof(string) != "string") {
-        throw new Error("Type Error: Parameter not a string.");
-    }
-    else if (string.length == 0){
-        throw new Error("Length Error: String is empty.");
-    }
-    else if (string.replace(/\s/g, '') == '') {
-        throw new Error("Length Error: String is full of spaces.");
-    }
-    else {
-        var newWord = '' + string.charAt(0);
-        var x = ["*", "$"];
-        let f = string.charAt(0).toLowerCase();
-        var count = 0;
+    // -- Error Checking --
+    if(!string) throw ": Missing parameter 'string'";
+    if(!validString(string)) throw ": Invalid String.";
+    
+    // -- Initializing Variables --
+    let newWord = '', count = 0;
 
-        for(var i = 1; i < string.length; i++) {
-            
-            if(f == string.charAt(i).toLowerCase()) {
-                if(count%2 == 0) {
-                    newWord = newWord + x[0];
-                }
-                else {
-                    newWord = newWord + x[1];
-                }
-                count++;
-                // console.log(newWord);
-            }
-            else {
-                newWord = newWord + string.charAt(i);
-                // console.log(newWord);
-            }
+    // -- Interating and Replacing Characters --
+    for(x of string.slice(1)) {
+        if(string[0].toLowerCase() == x.toLowerCase()) {
+            count%2 == 0 ? newWord += '*' : newWord += '$';
+            count++;
+            continue;
         }
-
-        return newWord;
+        newWord += x;
     }
+
+    return string[0] + newWord;
 }
 
-// console.log(replaceChar("Daddy"));
-// console.log(replaceChar("Mommy"));
-// console.log(replaceChar("Hello, How are you? I hope you are well"));
-// console.log(replaceChar("babbbbble"));
-// console.log(replaceChar(""));
-// console.log(replaceChar(123));
-
+/*
+    Function mashUp takes in two String Parameters.
+    Returns the concatenation of the two strings, separated by a space and 
+    swapping the first 2 characters of each.
+*/
 const mashUp = function mashUp(string1, string2) {
-    if(string1 === undefined || string2 === undefined) {
-        throw new Error("Existential Error: String does not exist");
-    }
-    else if (typeof(string1) != "string" || typeof(string2) != "string") {
-        throw new Error("Type Error: Input invalid!");
-    }
-    else if (string1.length < 2 || string2.length < 2) {
-        throw new Error("Length Error: Strings must be greater than 2.");
-    }
-    else if (string1.replace(/\s/g, '').length < 2 || string2.replace(/\s/g, '').length < 2) {
-        throw new Error("Length Error: String is full of spaces. >:(");
-    }
-    else {
-        var s1 = string1.slice(0, 2) + string2.slice(2);
-        var s2 = string2.slice(0, 2) + string1.slice(2);
-        var s3 = s2 + " " + s1;
+    // -- Error Checking --
+    if(!string1) throw ": Missing Parameter 'string1'";
+    if(!string2) throw ": Missing Parameter 'string2'";
+    if(!validString(string1)) throw ": Invalid String.";
+    if(!validString(string2)) throw ": Invalid String.";
+    if(string1.length < 2) throw ": Invalid Input, Length must be at least 2 characters."
+    if(string2.length < 2) throw ": Invalid Input, Length must be at least 2 characters."
+    
+    // -- Slicing and Concatenating --
+    let s1 = string1.slice(0, 2) + string2.slice(2);
+    let s2 = string2.slice(0, 2) + string1.slice(2);
 
-        return s3;
-    }
+    return s2 + " " + s1;
 }
-
-// console.log(mashUp("Patrick", "Hill"));
-// console.log(mashUp("hello", "world"));
-// console.log(mashUp("Patrick", ""));
-// console.log(mashUp());
-// console.log(mashUp("John"));
-// console.log(mashUp ("h", "Hello"));  // Throws Error
-// console.log(mashUp ("h","e"));       // Throws Error
-
 
 module.exports = {
     firstName: "CINDY", 
